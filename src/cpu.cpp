e24ee453eb011c0510dd8e74ebf0037a97a1c062
nl937 <nathanlong937@gmail.com>
2022-12-13 07:21:01 -0500

    Update cpu.cpp

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,7 +1,7 @@
 /*Nathan L. 2020
 This file is part of C8-emu
 
-C8-emuis free software: you can redistribute it and/or modify
+C8-emu is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
